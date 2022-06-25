27077f79adef47232a102841308bbb2c137baf9a
barseek <sergey.b@hey.com>
2022-06-25 14:05:25 +0300

    Unprocessable entity handling

--- app/blueprints/cart_item_blueprint.rb
+++ app/blueprints/cart_item_blueprint.rb
@@ -2,6 +2,7 @@ class CartItemBlueprint < Blueprinter::Base
   identifier :id
 
   fields :quantity
+
   view :normal do
     association :cart, blueprint: CartBlueprint
     association :product, blueprint: ProductBlueprint
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    invalid(e)
+  end
 
   def health
-    render json: { message: "All's good" }, status: 200
+    render json: { message: "All's good" }, status: :ok
   end
 
   private
 
   def not_found
-    render json: { message: "Not found" }, status: 404
+    render json: { message: "Not found" }, status: :not_found
+  end
+
+  def invalid(e)
+    render json: { message: e.message }, status: :unprocessable_entity
   end
 end
